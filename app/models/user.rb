d4d984f2dafd5b88bf8531d8731323a5c650a4f8
Mario Merendino <mario.merendino@vimeo.com>
2021-02-21 15:45:48 -0500

    Beginning of followers.
    There might be a much easier approach, but this is good for now...

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,18 @@ class User < ApplicationRecord
   validates_uniqueness_of :username
 
   has_many :tweets
+  has_many :follows
+
+  # returns a list of all the users followers
+  def followers
+    follower_ids = Follow.where(following_id: id).pluck(:user_id)
+    User.where(id: follower_ids)
+  end
+
+  # returns a list of all the users this user is following
+  def following
+    # collect all the users ids that this user is following
+    following_ids = follows.pluck(:following_id)
+    User.where(id: following_ids)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_21_174736) do
+ActiveRecord::Schema.define(version: 2021_02_21_180113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "following_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.integer "user_id"
