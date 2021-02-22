3f742d4ae0f4751267aea77dd925aeca63a2c78e
Mario Merendino <mario.merendino@vimeo.com>
2021-02-22 01:53:04 -0500

    Tweets controller uses the timeline service class

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,5 @@
 class TweetsController < ApplicationController
+  def index
+    render json: { tweets: Timeline.new(current_user).execute }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :sessions, only: [:create]
   resources :users, only: [:create]
   post '/login', to: 'users#login'
+
+  resources :tweets, only: [:index]
 
   root to: 'static#home'
 end
